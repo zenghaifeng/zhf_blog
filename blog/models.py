5f854ff59856c2d5f62c68740c98ab5d239e018d
your name <your email>
2018-04-05 14:33:43 +0800

    first commit

---
+++ blog/models.py
@@ -0,0 +1,63 @@
+#coding:utf-8
+from __future__ import unicode_literals
+
+from django.db import models
+import markdown
+# Create your models here.
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.utils.six import python_2_unicode_compatible
+from django.utils.html import strip_tags
+class Category(models.Model):
+    
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+@python_2_unicode_compatible
+class Tag(models.Model):
+    
+    name = models.CharField(max_length=100)
+    def __str__(self):
+       return self.name
+@python_2_unicode_compatible
+class Post(models.Model):
+   
+    title = models.CharField(max_length=70)
+
+    body = models.TextField()
+
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+   
+    excerpt = models.CharField(max_length=200, blank=True)
+
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    author = models.ForeignKey(User)
+    views = models.PositiveIntegerField(default=0)
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+    
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+    
+    def save(self, *args, **kwargs):    
+       
+        if not self.excerpt:
+           
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+           
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        super(Post, self).save(*args, **kwargs)       
+
+    class Meta:
+        ordering = ['-created_time']
---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+from django import template
+from ..models import Post, Category, Tag
+from django.db.models.aggregates import Count
+register = template.Library()
+
+@register.simple_tag
+def get_recent_posts(num=5):
+    return Post.objects.all().order_by('-created_time')[:num]
+
+
+
+@register.simple_tag    
+def archives():
+    return Post.objects.dates('created_time', 'month', order='DESC')
+
+@register.simple_tag
+def get_categories():
+    # 别忘了在顶部引入 Category 类
+    return Category.objects.annotate(num_posts=Count('post'))
+
+
+@register.simple_tag
+def get_tags():
+   	return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
